01e23ffc33b3e86a5e3392e6cd256210d3604e69
Fernando Bunn <bunn@idevzilla.com>
2020-07-24 22:49:32 +0100

    Add basic server implementation

--- pi_monitor.py
+++ pi_monitor.py
@@ -1,38 +1,63 @@
-
 import subprocess
 import re
 import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-def get_thermal_temperature():
-	thermal = subprocess.check_output("cat /sys/class/thermal/thermal_zone0/temp", shell=True)
-	return float(thermal) / 1000.0
 
-#returns the temperature of the SoC as measured by the on-board temperature sensor
-def get_soc_temperature():
-	temp = subprocess.check_output("vcgencmd measure_temp", shell=True)
-	return re.findall(r'\d+\.\d+', temp)[0]
+class Monitor:
 
-#uptime in seconds
-def get_uptime(): 
-	uptime = subprocess.check_output("cat /proc/uptime", shell=True)
-	return float(uptime.split(" ")[0])
+    def get_thermal_temperature(self):
+        thermal = subprocess.check_output(
+            "cat /sys/class/thermal/thermal_zone0/temp", shell=True).decode("utf8")
+        return float(thermal) / 1000.0
 
-#returns load averages for 1, 5, and 15 minutes
-def get_load_average():
-	uptime = subprocess.check_output("uptime", shell=True)
-	load_average = uptime.split("load average:")[1].split(",")
-	return list(map(float, load_average))
+    # returns the temperature of the SoC as measured by the on-board temperature sensor
+    def get_soc_temperature(self):
+        temp = subprocess.check_output(
+            "vcgencmd measure_temp", shell=True).decode("utf8")
+        return float(re.findall(r'\d+\.\d+', temp)[0])
 
-def get_kernel_release():
-	return subprocess.check_output("uname -r", shell=True)
+    # uptime in seconds
+    def get_uptime(self):
+        uptime = subprocess.check_output(
+            "cat /proc/uptime", shell=True).decode("utf8")
+        return float(uptime.split(" ")[0])
 
-def get_monitor_json():
-	data = {
-		"soc_temperature": get_soc_temperature(),
-		"uptime": get_uptime(),
-		"load_average": get_load_average(),
-		"kernel_release": get_kernel_release()
-		}
-	return json.dumps(data)
+    # returns load averages for 1, 5, and 15 minutes
+    def get_load_average(self):
+        uptime = subprocess.check_output("uptime", shell=True).decode("utf8")
+        load_average = uptime.split("load average:")[1].split(",")
+        return list(map(float, load_average))
 
-print(get_monitor_json())
+    def get_kernel_release(self):
+        return subprocess.check_output("uname -r", shell=True).decode("utf8").strip()
+
+    def get_json(self):
+        data = {
+            "soc_temperature": self.get_soc_temperature(),
+            "uptime": self.get_uptime(),
+            "load_average": self.get_load_average(),
+            "kernel_release": self.get_kernel_release()
+        }
+        return json.dumps(data)
+
+
+class MonitorServer(BaseHTTPRequestHandler):
+
+    def set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+    def do_GET(self):
+        self.set_response()
+        print(self.path)
+        if self.path == "/monitor.json" or self.path == "/monitor":
+            response = Monitor().get_json().encode()
+            self.wfile.write(response)
+
+
+port = 8088
+server_address = ('', port)
+httpd = HTTPServer(server_address, MonitorServer)
+httpd.serve_forever()
