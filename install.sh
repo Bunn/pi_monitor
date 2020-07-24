bc2a1aff9e690269e83950f5ea24ef613686c38b
Fernando Bunn <bunn@idevzilla.com>
2020-07-24 23:41:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ wget https://raw.githubusercontent.com/Bunn/pi_monitor/master/pi-monitor.service
 echo ""
 echo "Moving files..."
 echo ""
-mv pi_monitor.py /usr/local/bin/
+mv pi-monitor.py /usr/local/bin/
 mv pi-monitor.service /etc/systemd/system/
 
 echo ""
