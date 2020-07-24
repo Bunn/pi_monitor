7c7e1c7bc04654cc1104dc5bfb47f490b6ae0bee
Fernando Bunn <bunn@idevzilla.com>
2020-07-24 23:39:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+echo ""
 echo "Downloading files..."
+echo ""
 wget https://raw.githubusercontent.com/Bunn/pi_monitor/master/pi-monitor.py
 wget https://raw.githubusercontent.com/Bunn/pi_monitor/master/pi-monitor.service
 
+echo ""
 echo "Moving files..."
+echo ""
 mv pi_monitor.py /usr/local/bin/
 mv pi-monitor.service /etc/systemd/system/
 
+echo ""
 echo "Starting pi-monitor service..."
+echo ""
 systemctl start pi-monitor.service
