65b0ac8d29eff0a1a04f9f98a18bd36582e6e6fd
Fernando Bunn <bunn@idevzilla.com>
2020-08-01 19:16:13 +0100

    update install file

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+systemctl stop pi-monitor.service
+
 echo ""
 echo "Downloading files..."
 echo ""
 wget https://raw.githubusercontent.com/Bunn/pi_monitor/master/pi-monitor.py
 wget https://raw.githubusercontent.com/Bunn/pi_monitor/master/pi-monitor.service
+
+echo ""
+echo "Removing old files files..."
+echo ""
+rm /usr/local/bin/pi-monitor.py
+rm /etc/systemd/system/pi-monitor.service
 
 echo ""
 echo "Moving files..."
@@ -14,5 +22,5 @@ mv pi-monitor.service /etc/systemd/system/
 echo ""
 echo "Starting pi-monitor service..."
 echo ""
-systemctl daemon-reload
 systemctl start pi-monitor.service
+systemctl enable pi-monitor.service 
